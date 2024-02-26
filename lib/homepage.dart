16259b470cad02a83a4c895ca7755c09f9fb40e4
Abithashree <you@example.com>
2024-02-26 19:35:46 +0530

    creates ui

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,21 +11,43 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 228, 241, 221),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Happy learning'),
-            SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'login_page');
-                },
-                child: Text('Back to login'))
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Color.fromARGB(255, 228, 241, 221),
+        body: SafeArea(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // children: [
+            //   Text('Happy learning'),
+            //   SizedBox(height: 20),
+            //   ElevatedButton(
+            //       onPressed: () {
+            //         Navigator.pushNamed(context, 'login_page');
+            //       },
+            //       child: Text('Back to login')),
+            children: [
+              Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(children: [
+                          SizedBox(
+                            width: 60,
+                            height: 60,
+                            child: Image.asset(
+                              'assets/images/images.png',
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text("Hello"),
+                              Text("Abitha"),
+                            ],
+                          ),
+                        ]),
+                        Icon(Icons.alarm),
+                      ]))
+            ],
+          ),
+        ));
   }
 }
