7c9a143c40a046d65e727267e162c0d146904603
Abithashree <you@example.com>
2024-02-26 20:52:29 +0530

    modified ui

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,43 +11,69 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 228, 241, 221),
+        //backgroundColor: Color.fromARGB(255, 228, 241, 221),
         body: SafeArea(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // children: [
-            //   Text('Happy learning'),
-            //   SizedBox(height: 20),
-            //   ElevatedButton(
-            //       onPressed: () {
-            //         Navigator.pushNamed(context, 'login_page');
-            //       },
-            //       child: Text('Back to login')),
-            children: [
-              Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+      child: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.fromLTRB(10, 30, 10, 30),
+              child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: const Color.fromARGB(255, 165, 202, 232),
+                  ),
                   child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Row(children: [
-                          SizedBox(
-                            width: 60,
-                            height: 60,
-                            child: Image.asset(
-                              'assets/images/images.png',
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: SizedBox(
+                              width: 60,
+                              height: 60,
+                              child: Image.asset(
+                                'assets/images/images.png',
+                              ),
                             ),
                           ),
-                          Column(
-                            children: [
-                              Text("Hello"),
-                              Text("Abitha"),
-                            ],
+                          const Column(
+                            children: [Text("Hello"), Text("Abitha")],
                           ),
                         ]),
-                        Icon(Icons.alarm),
-                      ]))
-            ],
-          ),
-        ));
+                        Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(Icons.alarm)),
+                      ]))),
+          SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'login_page');
+              },
+              child: Text('Back to login')),
+        ],
+      ),
+    ));
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mainAxisAlignment: MainAxisAlignment.center,
+        // children: [
+        //   Text('Happy learning'),
+        //   SizedBox(height: 20),
+        //   ElevatedButton(
+        //       onPressed: () {
+        //         Navigator.pushNamed(context, 'login_page');
+        //       },
+        //       child: Text('Back to login')),
